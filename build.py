bf00ae6d7f0a852c592d8337acc74c09b8177861
utk1801 <utk1801@users.noreply.github.com>
2017-06-13 18:30:10 +0000

    Done

--- build.py
+++ build.py
@@ -2,6 +2,5 @@ import numpy as np
 
 
 def solution(array1, array2):
-    """
-    Enter your code here
-    """
+    return np.intersect1d(array1,array2)
+#print solution([0,10,20,40,60],[10,30,40])    
