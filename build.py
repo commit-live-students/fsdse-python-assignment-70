5663e8cad363c4b67f6b4eb1065b27e0ce1f2321
ramkrishan-sahu <ram04190@gmail.com>
2017-06-25 02:21:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-
 def solution(array1, array2):
-    """
-    Enter your code here
-    """
+    lis = []
+    for i in array1:
+        for j in array2:
+            if (i == j):
+                lis.append(i)
+    return lis
