d16628498288c92576414727fdb8472ce0088976
nixen420 <nikhil.singh4008@gmail.com>
2017-06-25 05:59:54 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+array1=[0,10,20,40,60]
+array2=[10,30,40]
 
 def solution(array1, array2):
-    """
-    Enter your code here
-    """
+    common=np.intersect1d(array1, array2)
+    return common
+solution(array1,array2)
